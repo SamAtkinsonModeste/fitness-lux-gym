e996dafe44588b2e467067b0f3f55195a123379a
Samantha Atkinson <sammodeste@yahoo.com>
2025-02-14 09:55:31 +0000

    bug: 500 from Heroku

--- luxgym/urls.py
+++ luxgym/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
 
-    path('', include('luxclasses.urls')),
     path('admin/', admin.site.urls),
+    path('', include('luxclasses.urls')),
 
 ]
