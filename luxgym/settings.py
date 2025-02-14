09c2ef52b4430a59f346bd3f5dabe50920f4bbc6
Samantha Atkinson <sammodeste@yahoo.com>
2025-02-14 09:43:03 +0000

    fix: url pattern in project urls.py and satitc file paths

--- luxgym/settings.py
+++ luxgym/settings.py
@@ -139,9 +139,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# Only if using collectstatic (especially for production):
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# If needed for additional static files during development:
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
