1b6ad187edb9e2d80cc9f4464ff83fc9ce0f6f4c
Samantha Atkinson <sammodeste@yahoo.com>
2025-02-14 02:58:36 +0000

    config:allowed host to settings.py as well as luxclass app

--- luxgym/settings.py
+++ luxgym/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kp@ec5+q8sapnpqdn8=t=7$aaejl38sr63)0!60xs#k2wqlp%s'
+SECRET_KEY = 'django-insecure-*332c9@jv1+lru*s7ee+_s3026#0(@h=_$-b7ng*$bn_gq+7&f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'luxclasses',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
