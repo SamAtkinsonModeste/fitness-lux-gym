d53fc0f1f542318205fe04628a0cf475792e5338
Samantha Atkinson <sammodeste@yahoo.com>
2025-02-16 17:27:59 +0000

    views: created a ListView for my FitnessTimatable model

--- timetablelux/views.py
+++ timetablelux/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import FitnessTimetable
 
 # Create your views here.
+
+
+class FitnessTimetableView(ListView):
+    model = FitnessTimetable
+    context_object_name = "fitnesstimetable"
