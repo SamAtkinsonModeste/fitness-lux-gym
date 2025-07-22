3e48497ed15d715483358919da0c6f35f1db50ad
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-07-22 14:55:50 +0100

    refactor: model: Renamed from LuxGymClassTimetable to ScheduledClass
    Created a variable `DAYS` to store a list of weekday choices (e.g., Monday to Sunday). This variable is now used as the `choices` argument for the `day` attribute in the `ScheduledClass` model. The `day` field is a `CharField` that restricts input to valid days of the week.

--- gymtimetable/models.py
+++ gymtimetable/models.py
@@ -4,23 +4,32 @@ from luxclasses.models import FitnessClasses
 
 # Create your models here.
 
-STATUS = (("0", "Draft"), ("1", "Publish"))
+
+DAYS_OF_WEEK = [
+    ('Monday', 'Monday'),
+    ('Tuesday', 'Tuesday'),
+    ('Wednesday', 'Wednesday'),
+    ('Thursday', 'Thursday'),
+    ('Friday', 'Friday'),
+    ('Saturday', 'Saturday'),
+    ('Sunday', 'Sunday'),
+]
 
 
-class LuxGymTimetable(models.Model):
+class ScheduledClass(models.Model):
     gymclass_organiser = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, related_name="gymtimetable_organiser")
     gym_class = models.ForeignKey(
         FitnessClasses, on_delete=models.SET_NULL, null=True, related_name="gym_classes")
-    day = models.CharField(max_length=50)
-    gym_class_time = models.DateTimeField()
+    day = models.CharField(choices=DAYS_OF_WEEK, default="Monday")
+    gym_class_time = models.TimeField()
     teacher = models.CharField(max_length=200)
     gym_class_duration = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now=True)
-    status = models.CharField(choices=STATUS, default="1")
+
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ['day','gym_class_time','created_on']
 
     def __str__(self):
         return f"{self.day} - Teacher is {self.teacher}"
