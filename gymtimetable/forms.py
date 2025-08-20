e5990a98df8fe2a0afc1624a2acd160a52d5cb79
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-08-20 21:08:47 +0100

    refactor(form): add Crispy grid for ScheduledClassForm
    - Add FormHelper to control rendering with Bootstrap 5 - Disable helper form_tag to use template <form> tag - Arrange fields in 2-column grid with Row/Column - Add g-3 spacing between rows - Center final single field with offset-md-3

--- gymtimetable/forms.py
+++ gymtimetable/forms.py
@@ -1,9 +1,35 @@
-from .models import ScheduledClass
 from django import forms
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column
+from .models import ScheduledClass
 
 class ScheduledClassForm(forms.ModelForm):
     class Meta:
         model = ScheduledClass
-        fields = ["gymclass_organiser", "gym_class", "day",
-                  "gym_class_time", "teacher", "gym_class_duration"]
+        fields = ['gym_class', 'day', 'gym_class_time', 'teacher', 'gym_class_duration']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Crispy form helper
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+
+        # Layout: 2 fields per row
+        self.helper.layout = Layout(
+            Row(
+                Column('day', css_class='col-md-6'),
+                Column('gym_class', css_class='col-md-6'),
+                css_class='g-3'
+            ),
+            Row(
+                Column('teacher', css_class='col-md-6'),
+                Column('gym_class_time', css_class='col-md-6'),
+                css_class='g-3'
+            ),
+            Row(
+                Column('gym_class_duration', css_class='col-md-6 mx-auto'),
+                css_class='g-3'
+            ),
+
+        )
