a63c1dfae172b29473dd74bba5f13c3cab9c5e29
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-08-20 19:26:27 +0100

    refactor(model): ScheduledClass -  day/duration and updated timestamps
    - Refactored the ScheduledClass model to improve clarity and maintainability. - Added `DAYS_OF_WEEK` and `CLASS_DURATION_TIME` constants to define valid choices for the `day` and `gym_class_duration` fields. - Changed both `day` and `gym_class_duration` from text-based fields to `IntegerField` with choices. - Updated the `created_on` field to use `auto_now_add=True` for automatic timestamping on creation. - Added a new `updated_on` field with `auto_now=True` to track record updates.

--- gymtimetable/models.py
+++ gymtimetable/models.py
@@ -5,15 +5,22 @@ from luxclasses.models import FitnessClasses
 # Create your models here.
 
 
-DAYS_OF_WEEK = [
-    ('Monday', 'Monday'),
-    ('Tuesday', 'Tuesday'),
-    ('Wednesday', 'Wednesday'),
-    ('Thursday', 'Thursday'),
-    ('Friday', 'Friday'),
-    ('Saturday', 'Saturday'),
-    ('Sunday', 'Sunday'),
-]
+DAYS_OF_WEEK = (
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thursday'),
+    (4, 'Friday'),
+    (5, 'Saturday'),
+    (6, 'Sunday'),
+)
+
+CLASS_DURATION_TIME = (
+    (0, '30 Minutes'),
+    (1, '45 Minutes'),
+    (2, '50 Minutes'),
+    (3, '60 Minutes'),
+)
 
 
 class ScheduledClass(models.Model):
@@ -21,15 +28,16 @@ class ScheduledClass(models.Model):
         User, on_delete=models.SET_NULL, null=True, related_name="gymtimetable_organiser")
     gym_class = models.ForeignKey(
         FitnessClasses, on_delete=models.SET_NULL, null=True, related_name="gym_classes")
-    day = models.CharField(choices=DAYS_OF_WEEK, default="Monday")
+    day = models.IntegerField(choices=DAYS_OF_WEEK, default=0)
     gym_class_time = models.TimeField()
     teacher = models.CharField(max_length=200)
-    gym_class_duration = models.CharField(max_length=100)
-    created_on = models.DateTimeField(auto_now=True)
+    gym_class_duration = models.IntegerField(choices=CLASS_DURATION_TIME, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
 
     class Meta:
         ordering = ['day','gym_class_time','created_on']
 
     def __str__(self):
-        return f"{self.day} - Teacher is {self.teacher}"
+        return f"{self.get_day_display()} - Teacher is {self.teacher}"
