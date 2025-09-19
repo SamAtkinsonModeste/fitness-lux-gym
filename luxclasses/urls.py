61ef411835f013d961ef3d0cc00500721b5d4ef7
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-09-19 10:56:24 +0100

    feat(urls): added using int:pk to FitnessClassDetailView

--- luxclasses/urls.py
+++ luxclasses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import FitnessClassesView
+from .views import FitnessClassesView, FitnessClassDetailView
 
+app_name = "luxclasses"
 
 urlpatterns = [
-    path('', FitnessClassesView.as_view(), name='home'),
+    path("", FitnessClassesView.as_view(), name="home"),
+    path("<int:pk>/", FitnessClassDetailView.as_view(), name="class_detail"),
 ]
