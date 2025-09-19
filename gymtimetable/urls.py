47607264bb8e5c76278bda005f5e9eabd835fbec
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-09-19 22:19:49 +0100

    feat(urls): add paths so user can book classes
    add paths for UserGymTimetableListView and toggle_booking

--- gymtimetable/urls.py
+++ gymtimetable/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import ScheduledClassListView
+from .views import (
+    ScheduledClassListView,
+    UserGymTimetableListView,
+    toggle_booking,
+)
+
+app_name = "gymtimetable"
 
 urlpatterns = [
-
-    path("", ScheduledClassListView.as_view(), name="gymtimetable"),
+    path("admin/", ScheduledClassListView.as_view(), name="admin_timetable"),
+    path("my/", UserGymTimetableListView.as_view(), name="class_list"),
+    path("book/<int:pk>/", toggle_booking, name="toggle_booking"),
 ]
