13e59d5be7795488f0982b295d00ad636ccafbb9
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-09-19 11:48:01 +0100

    refactor(urls): introduce app namespaces
    namespaces for luxclasses & gymtimetable and update reverses

--- luxgym/urls.py
+++ luxgym/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("gymtimetable/", include("gymtimetable.urls")),
+    path(
+        "gymtimetable/", include("gymtimetable.urls", namespace="gymtimetable")
+    ),
     path("accounts/", include("allauth.urls")),
-    path('admin/', admin.site.urls),
-    path('', include("luxclasses.urls")),
-
+    path("admin/", admin.site.urls),
+    path("", include("luxclasses.urls", namespace="luxclasses")),
 ]
